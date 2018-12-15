a4e01dd5a1a6e4b70a06e0de480556465083ffed
User <User@User-PC>
2018-12-15 09:35:35 +0530

    added drivers and selenium code

--- TestApp/src/main/java/com/test/scenarios/Demo.java
+++ TestApp/src/main/java/com/test/scenarios/Demo.java
@@ -1,10 +1,20 @@
 package com.test.scenarios;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Demo {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world !");
-		
-		System.out.println("This is test");
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("oneplus 6T");
+		driver.findElement(By.id("btnK")).sendKeys(Keys.RETURN);
+		Thread.sleep(5000);
+		driver.close();
 	}
 }
