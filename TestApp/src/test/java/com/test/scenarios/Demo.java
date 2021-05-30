925c4a9fa2e76885012b38fbc048441e815f4b96
vinay.kami <vinaykkumar1987@gmail.com>
2021-05-30 20:26:42 +0530

    xccxcx

--- TestApp/src/test/java/com/test/scenarios/Demo.java
+++ TestApp/src/test/java/com/test/scenarios/Demo.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on origin/master
 package com.test.scenarios;
 
 import org.openqa.selenium.By;
@@ -18,3 +19,73 @@ public class Demo extends ProjectUtils {
 	}
 
 }
+=======
+package com.test.scenarios;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import com.test.utils.ProjectUtils;
+
+public class Demo extends ProjectUtils {
+
+	@Test(description = "launch google and search")
+	public void googleSearch() {
+
+		getDriver().get("https://www.bing.com");
+		
+		getDriver().manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+		List<WebElement> links = getDriver().findElements(By.tagName("a"));
+
+		System.out.println("No of links present in bing is: " + links.size());
+
+	}
+
+	@Test
+	public void amazonTest() {
+
+		getDriver().get("https://www.amazon.in/");
+		
+		getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys("Refrigerators");
+		
+		getDriver().findElement(By.xpath("//*[@id='nav-search-submit-text']/following-sibling::input")).click();
+		
+		WebDriverWait wait = new WebDriverWait(getDriver(),20);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated
+        		(By.xpath("//span[contains(text(),'refrigerator')]")));
+             // click on the compose button as soon as the "compose" button is visible
+        getDriver().findElement(By.xpath("//div[contains(text(),'COMPOSE')]")).click();
+		
+		
+	}
+	
+	@Test
+	public void flipkartTest() {
+		
+		getDriver().get("https://www.amazon.in/");
+		getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys("Refrigerators");
+		getDriver().findElement(By.xpath("//*[@id='nav-search-submit-text']/following-sibling::input")).click();
+	}
+	
+	@Test
+	
+
+	@AfterClass
+	public void tearDown() {
+
+		getDriver().quit();
+	}
+
+
+}
+>>>>>>> e39bdeb xccxcx
