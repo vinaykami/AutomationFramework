020eda59d3c88914cadbc0ffd8159c789eeec623
vinaykami <vinaykkumar1987@gmail.com>
2021-05-30 19:49:10 +0530

    added new class SampleTest.java

--- app-web-ui/src/test/java/com/test/NewTest.java
+++ app-web-ui/src/test/java/com/test/NewTest.java
@@ -3,9 +3,16 @@ package com.test;
 import org.testng.annotations.Test;
 
 public class NewTest {
-  @Test
-  public void f() {
-	  
-	  System.out.println("this is test ng demo");
-  }
+	@Test
+	public void testMeth1() {
+
+		System.out.println("this is test ng demo");
+	}
+
+	@Test
+	public void testMeth2() {
+
+		System.out.println("this is test ng demo");
+	}
+
 }
