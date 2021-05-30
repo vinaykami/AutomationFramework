b5ea9827b49b3a0ec058ca76c3eff915971bcec4
vinay.kamireddy <vinay.kamireddy@192.168.29.49>
2021-05-30 20:15:45 +0530

    created new project app-desktop-ui

---
+++ app-desktop-ui/src/test/java/com/test/NewTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test
+	public void testMeth1() {
+
+		System.out.println("this is test ng demo");
+	}
+
+	@Test
+	public void testMeth2() {
+
+		System.out.println("this is test ng demo");
+	}
+
+}
